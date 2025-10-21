71b9aeefa01c343d0fd60a416795b8d80b415d90
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 16:37:08 +0200

    opened menu component

---
+++ src/components/navigation/OpenedMenu.tsx
@@ -0,0 +1,33 @@
+import NavigationItem from "./NavigationItem";
+import { type NavItem } from "../../types";
+import { useTranslation } from "react-i18next";
+import ImageProps from "../../props/ImageProps";
+import { closeMenu } from "../../data";
+import { myBoxOpenedMenu } from "../../data";
+
+type OpenedMenuProps = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+function OpenedMenu({ isOpen, onClose }: OpenedMenuProps) {
+  const { t } = useTranslation();
+  const footerNavigation = t("footerNav", { returnObjects: true }) as NavItem[];
+
+  return (
+    <section className={`opened-menu ${isOpen ? "open" : ""}`}>
+      <section className="opened-menu-logo">
+        <ImageProps data={myBoxOpenedMenu} />
+      </section>
+      <section className="close-menu" onClick={onClose}>
+        <ImageProps data={closeMenu} />
+      </section>
+
+      <section className="opened-menu-wrapper">
+        <NavigationItem data={footerNavigation} />
+      </section>
+    </section>
+  );
+}
+
+export default OpenedMenu;
