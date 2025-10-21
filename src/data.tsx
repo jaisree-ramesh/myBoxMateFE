9d28b17ad677ecb4497da7cf325ad6894dbdc57b
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 16:34:56 +0200

    data file to keep the icons and images top be used with the ImageProps

---
+++ src/data.tsx
@@ -0,0 +1,28 @@
+import CloseMenu from "./assets/icons/material-symbols--close-rounded.svg";
+import MyBoxImage from "./assets/images/my-box-mate-opened-menu.png";
+import MyMateBoxLogo from "./assets/images/my-boy-mate-logo.png";
+
+export const closeMenu = [
+  {
+    id: 1,
+    image: CloseMenu,
+    alt: "My Box Mate - close menu",
+  },
+];
+
+export const myBoxOpenedMenu = [
+  {
+    id: 1,
+    image: MyBoxImage,
+    alt: "My Box Mate - opened menu Logo",
+  },
+];
+
+export const logoImage = [
+  {
+    id: 1,
+    link: "/",
+    image: MyMateBoxLogo,
+    alt: "My Box Mate Logo",
+  },
+];
