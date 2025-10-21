9c5a9e784fb9a2b281c3607f266eb56f53d7cdd0
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 12:43:44 +0200

    i18n file added to the main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@/styles/style.sass";
+import "../118n.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
