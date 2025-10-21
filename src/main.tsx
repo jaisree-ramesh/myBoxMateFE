a590c35b1265b82c97a0ccfd97295501b934ff10
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 21:08:43 +0200

    edit: file name

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import "@/styles/style.sass";
-import "../118n.ts";
+import "../i18n.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
