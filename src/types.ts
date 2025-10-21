1cb19a3e075947d7485fc5233982861a08a970bc
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 12:43:54 +0200

    types file

---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface IUser {
+  _id: string;
+  username: string;
+  email: string;
+  token?: string; // JWT token returned on login/register
+}
+
+export interface IItem {
+  _id: string;
+  name: string;
+  desc?: string;
+  box?: string; // ObjectId of parent box
+  parentId?: string; // undefined for boxes, box id for child items
+  owner?: string; // user id of owner
+  collaborators?: string[]; // array of user ids
+  createdBy?: string;
+  editedBy?: string;
+  image?: string; // Cloudinary URL
+  qrCode?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export type NavItem = {
+  id?: number;
+  name: string;
+  link: string;
+  ariaLabel?: string;
+};
+
+export type NavigationItemProps = {
+  data: NavItem[];
+};
+
+export interface ImageItem {
+  link: string;
+  image: string;
+  alt: string;
+  id?: string;
+}
