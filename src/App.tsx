4bf615c193024366890d5f76baefb3f03032c4e3
Francine-Pepe <franmelopepe@gmail.com>
2025-10-21 21:07:53 +0200

    Home component added to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import FooterNavigation from "./components/navigation/FooterNavigation";
 import HeaderNavigation from "./components/navigation/HeaderNavigation";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <div>
         <HeaderNavigation />
+        <Home />
         <FooterNavigation />
       </div>
     </>
