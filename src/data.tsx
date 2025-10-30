df112ecc3a695ff391e90216d19da1a1c637cd1c
Francine-Pepe <franmelopepe@gmail.com>
2025-10-30 15:42:17 +0100

    data: images that are used in the Spaces

--- src/data.tsx
+++ src/data.tsx
@@ -2,6 +2,15 @@ import CloseMenu from "./assets/icons/material-symbols--close-rounded.svg";
 import MyBoxImage from "./assets/images/my-box-mate-opened-menu.png";
 import MyMateBoxLogo from "./assets/images/my-boy-mate-logo.png";
 import MyBoxMateFront from "./assets/images/my-box-mate-header-image.jpg";
+import Closet from "./assets/icons/closet_.png";
+import Fridge from "./assets/icons/fridge_.png";
+import Garage from "./assets/icons/garage_.png";
+import Garden from "./assets/icons/garden_.png";
+import Kitchen from "./assets/icons/kitchen_.png";
+import MovingTruck from "./assets/icons/moving-truck_.png";
+import Package from "./assets/icons/package_.png";
+import Warehouse from "./assets/icons/warehouse_.png";
+import Add from "./assets/icons/add.png";
 
 export const closeMenu = [
   {
@@ -28,11 +37,30 @@ export const logoImage = [
   },
 ];
 
-export const homeImage =
-[
+export const homeImage = [
   {
     id: 1,
     image: MyBoxMateFront,
-    alt: "My Box Mate Front "
-  }
-]
+    alt: "My Box Mate Front ",
+  },
+];
+
+export const categoryIcons = [
+  { id: "Closet", image: Closet, alt: "Closet" },
+  { id: "Fridge", image: Fridge, alt: "Fridge" },
+  { id: "Garage", image: Garage, alt: "Garage" },
+  { id: "Garden", image: Garden, alt: "Garden" },
+  { id: "Kitchen", image: Kitchen, alt: "Kitchen" },
+  { id: "Moving", image: MovingTruck, alt: "Moving" },
+  { id: "Moving box", image: Package, alt: "Moving box" },
+  { id: "Basement", image: Warehouse, alt: "Basement" },
+  
+];
+
+export const AddIcon = [
+  {
+    id: "Create new room",
+    image: Add,
+    alt: "Create new room",
+  },
+];
