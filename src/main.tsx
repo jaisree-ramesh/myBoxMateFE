b447969e7b17706ec3a9c12de7fd094a0d131e8d
Francine-Pepe <franmelopepe@gmail.com>
2025-10-30 16:58:57 +0100

    qrcode component added to the main

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import "@/styles/style.sass";
 import "../i18n.ts";
+import QRCode from "react-qr-code";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <QRCode value="hey" />
   </StrictMode>
 );
