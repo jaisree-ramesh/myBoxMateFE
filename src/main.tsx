83e31d0df8ff82f4dc6b86cf8227abcafd1f7374
Francine-Pepe <franmelopepe@gmail.com>
2025-11-03 15:28:48 +0100

    qr code component was removed from main (it was just a test)

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import "@/styles/style.sass";
 import "../i18n.ts";
-import QRCode from "react-qr-code";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <QRCode value="hey" />
   </StrictMode>
 );
